c8b69f3ee6cd33af872141fa49683462aa153d5d
Denis <>
2023-07-25 12:51:51 +0300

    Change Server

--- scripts/start.js
+++ scripts/start.js
@@ -7,10 +7,10 @@ const { PORT = 3000 } = process.env
 const app = express()
 app.use(express.static('dist'))
 
-app.get('/', (req, res) => {
-    res.status(200).sendFile(path.resolve('dist/index.html'))
-    console.log('Yo Common!')
-})
+// app.get('/', (req, res) => {
+//     res.status(200).sendFile(path.resolve('dist/index.html'))
+//     console.log('Yo Common!')
+// })
 
 app.listen(PORT, () => {
     console.log(`Server is working on ${PORT} port`)
